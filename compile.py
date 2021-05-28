7b7fbea6c8db4869252047152b2fb174ffacd5b1
Mark Spanbroek <mark@spanbroek.net>
2021-05-28 09:43:13 +0200

    re-enable esp fix
    Was accidentally removed by latest pull request.

--- compile.py
+++ compile.py
@@ -5,20 +5,23 @@ Import("env")
 libdeps = env.subst("$PROJECT_LIBDEPS_DIR/$PIOENV")
 nimcache = env.subst("$PROJECT_SRC_DIR/nimcache")
 main = env.subst("$PROJECT_SRC_DIR/main.nim")
+cpu = "avr"
+if "espressif" in env.Dump():
+  cpu = "esp"
 
 flags = (
-    f"--path:{libdeps} "
-    f"--nimcache:{nimcache} "
-    "--compileOnly "
-    "--cpu:avr "
-    "--deadCodeElim "
-    "--os:standalone "
-    "--noMain "
-    "--gc:none "
-    "--stacktrace:off "
-    "--profiler:off"
+  f"--path:{libdeps} "
+  f"--nimcache:{nimcache} "
+  "--compileOnly "
+  f"--cpu:{cpu} "
+  "--deadCodeElim "
+  "--os:standalone "
+  "--noMain "
+  "--gc:none "
+  "--stacktrace:off "
+  "--profiler:off"
 )
 
 result = system(f"nim cpp {flags} {main}")
 if result != 0:
-    exit(result)
+  exit(result)
